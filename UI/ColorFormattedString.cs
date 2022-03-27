f3eb000637988b7f5deae4c287c16146625f36da
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-03-27 19:58:00 +0300

    Added ColorFormattedString

---
+++ UI/ColorFormattedString.cs
@@ -0,0 +1,39 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System.Linq;
+
+namespace Source1;
+
+[UseTemplate]
+public class ColorFormattedString : Panel
+{
+	public ColorFormattedString Clear()
+	{
+		DeleteChildren( true );
+		return this;
+	}
+
+	public ColorFormattedString AddText( string text )
+	{
+		Add.Label( text );
+		return this;
+	}
+
+	public ColorFormattedString AddText( string text, Color color )
+	{
+		color = color.WithAlpha( 1 );
+
+		var label = Add.Label( text );
+		label.Style.FontColor = color;
+		return this;
+	}
+
+	public ColorFormattedString AddText( string text, string color )
+	{
+		// add whitespaces
+		var label = Add.Label( text );
+		label.Style.Set( "color", color );
+		return this;
+	}
+}
