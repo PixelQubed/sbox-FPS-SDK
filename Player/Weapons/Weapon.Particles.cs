2d3e8fd15ff854f0736f59a72cc94eb3221c1f06
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-07-15 14:39:14 +0300

    CreatePersistentParticle

---
+++ Player/Weapons/Weapon.Particles.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Sandbox;
+
+namespace Amper.Source1;
+
+partial class Source1Weapon
+{
+	struct PersistentParticleData
+	{
+		public Entity Entity;
+		public string Path;
+		public string Attachment;
+	}
+
+	Dictionary<Particles, PersistentParticleData> PersistentParticles { get; set; } = new();
+
+	public Particles CreatePersistentParticle( string effect, string attachment )
+	{
+		var effectEntity = GetEffectEntity();
+		var particle = Particles.Create( effect, effectEntity, attachment );
+
+		if ( particle == null )
+			return null;
+
+		PersistentParticles[particle] = new()
+		{
+			Entity = effectEntity,
+			Path = effect,
+			Attachment = attachment
+		};
+
+		return particle;
+	}
+
+	public void DisposePersistentParticle( Particles effect )
+	{
+		if ( effect == null )
+			return;
+
+		effect.Destroy();
+		PersistentParticles.Remove( effect );
+	}
+
+	[Event.Frame]
+	public virtual void EffectThink()
+	{
+		if ( ShouldAutoDisposePersistentParticles() )
+		{
+			DisposeAllPesistentParticles();
+			return;
+		}
+
+		var effectEntity = GetEffectEntity();
+		foreach ( var pair in PersistentParticles )
+		{
+			var particle = pair.Key;
+			var data = pair.Value;
+
+			if ( data.Entity != effectEntity )
+			{
+				particle.SetEntityAttachment( 0, effectEntity, data.Attachment );
+				data.Entity = effectEntity;
+				PersistentParticles[particle] = data;
+			}
+		}
+	}
+
+	private bool ShouldAutoDisposePersistentParticles()
+	{
+		if ( PersistentParticles.Count == 0 )
+			return false;
+
+		if ( !IsValid )
+			return true;
+
+		if ( !IsDeployed )
+			return true;
+
+		if ( IsDormant )
+			return true;
+
+		var ownerPlayer = Player;
+		if ( !ownerPlayer.IsValid() )
+			return true;
+
+		return false;
+	}
+
+	public void DisposeAllPesistentParticles()
+	{
+		foreach ( var pair in PersistentParticles ) 
+			DisposePersistentParticle( pair.Key );
+	}
+}
