29f3669e435f69d469191bd682efbc63c9a41c17
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-07-16 13:41:56 +0300

    r_postprocess_force

--- PostProcessingManager.cs
+++ PostProcessingManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Sandbox;
 
 namespace Amper.Source1;
 
@@ -7,23 +8,41 @@ public class PostProcessingManager
 {
 	private const string EnabledCombo = "D_ENABLED";
 
-	private Dictionary<Type, BasePostProcess> Effects = new();
-	private Dictionary<Type, bool> EnabledCached = new();
+	Dictionary<Type, BasePostProcess> Effects = new();
+	Dictionary<Type, bool> EnabledCached = new();
+	Dictionary<Type, bool> ForceEnabled = new();
 
-	public virtual void FrameSimulate() { }
+	public void FrameSimulate()
+	{
+		Update();
+
+		foreach ( var pair in ForceEnabled )
+		{
+			var type = pair.Key;
+			var enabled = pair.Value;
+
+			SetVisible( type, enabled );
+		}
+	}
+
+	public virtual void Update() { }
 
 	public T GetOrCreate<T>() where T : BasePostProcess, new()
 	{
 		var type = typeof( T );
+		return (T)GetOrCreate( type );
+	}
 
+	public BasePostProcess GetOrCreate( Type type )
+	{
 		if ( Effects.TryGetValue( type, out var effect ) )
-			return (T)effect;
+			return effect;
 
-		effect = new T();
+		effect = TypeLibrary.Create<BasePostProcess>(type); 
 		Effects.Add( type, effect );
 		PostProcess.Add( effect );
 		SetVisible( effect, false );
-		return (T)effect;
+		return effect;
 	}
 
 	public bool IsVisible<T>() where T : BasePostProcess
@@ -56,5 +75,37 @@ public class PostProcessingManager
 
 		var type = effect.GetType();
 		EnabledCached[type] = visible;
+	}
+
+	public void SetVisible( Type type, bool visible )
+	{
+		var effect = GetOrCreate( type );
+		if ( effect == null )
+			return;
+
+		SetVisible( effect, visible );
+	}
+
+	public void SetForced( string name, bool enabled )
+	{
+		var type = TypeLibrary.GetTypeByName( name );
+		ForceEnabled[type] = enabled;
+	}
+
+	[ConCmd.Client( "r_postprocess_force" )]
+	public static void Command_ForcePostProcessing( string name, bool enabled )
+	{
+		var manager = GameRules.Current.PostProcessingManager;
+		if ( manager == null )
+			return;
+
+		try
+		{
+			manager.SetForced( name, enabled );
+			Log.Info( $"Changed \"{name}\" effect visibility: {enabled}." );
+		} catch
+		{
+			Log.Info( $"Failed to change visibility of \"{name}\" effect." );
+		}
 	}
 }
