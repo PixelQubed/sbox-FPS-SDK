7e40b030c322c6ec99545e46f9985da015cfce4e
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-07-21 00:54:06 +0300

    Create TalkerScript.cs

---
+++ Responses/TalkerScript.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox;
+
+namespace Amper.Source1;
+
+public partial class TalkerScript<T> : GameResource where T : Enum
+{
+	public T Cringe { get; set; }
+}
