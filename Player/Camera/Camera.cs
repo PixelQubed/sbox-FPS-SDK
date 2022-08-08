17823ae98250d580e1de40516c5a5b9dd6cda9b1
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-08-08 20:03:00 +0300

    cl_smoothstairs

--- Player/Camera/Camera.cs
+++ Player/Camera/Camera.cs
@@ -60,10 +60,12 @@ partial class Source1Camera : CameraMode
 		CalculateFieldOfView( player );
 	}
 
+
 	public virtual void CalculatePlayerView( Source1Player player )
 	{
 		var punch = player.ViewPunchAngle;
 		Rotation *= Rotation.From( punch.x, punch.y, punch.z );
+		SmoothViewOnStairs( player );
 
 		if( cl_thirdperson )
 		{
@@ -96,6 +98,66 @@ partial class Source1Camera : CameraMode
 			case ObserverMode.Deathcam:
 				CalculateDeathCamView( player );
 				break;
+		}
+	}
+
+
+	float m_flOldPlayerZ;
+	float m_flOldPlayerViewOffsetZ;
+	[ConVar.Client] public static bool cl_smoothstairs { get; set; }
+
+	public virtual void SmoothViewOnStairs( Source1Player player )
+	{
+		var pGroundEntity = player.GroundEntity;
+		float flCurrentPlayerZ = player.Position.z;
+		float flCurrentPlayerViewOffsetZ = player.EyeLocalPosition.z;
+
+		// Smooth out stair step ups
+		// NOTE: Don't want to do this when the ground entity is moving the player
+		if ( pGroundEntity.IsValid() && (flCurrentPlayerZ != m_flOldPlayerZ) && cl_smoothstairs &&
+			 m_flOldPlayerViewOffsetZ == flCurrentPlayerViewOffsetZ )
+		{
+			int dir = (flCurrentPlayerZ > m_flOldPlayerZ) ? 1 : -1;
+
+			float steptime = Time.Delta;
+			if ( steptime < 0 )
+			{
+				steptime = 0;
+			}
+
+			m_flOldPlayerZ += steptime * 150 * dir;
+
+			const float stepSize = 18.0f;
+
+			if ( dir > 0 )
+			{
+				if ( m_flOldPlayerZ > flCurrentPlayerZ )
+				{
+					m_flOldPlayerZ = flCurrentPlayerZ;
+				}
+				if ( flCurrentPlayerZ - m_flOldPlayerZ > stepSize )
+				{
+					m_flOldPlayerZ = flCurrentPlayerZ - stepSize;
+				}
+			}
+			else
+			{
+				if ( m_flOldPlayerZ < flCurrentPlayerZ )
+				{
+					m_flOldPlayerZ = flCurrentPlayerZ;
+				}
+				if ( flCurrentPlayerZ - m_flOldPlayerZ < -stepSize )
+				{
+					m_flOldPlayerZ = flCurrentPlayerZ + stepSize;
+				}
+			}
+
+			Position += Vector3.Up * (m_flOldPlayerZ - flCurrentPlayerZ);
+		}
+		else
+		{
+			m_flOldPlayerZ = flCurrentPlayerZ;
+			m_flOldPlayerViewOffsetZ = flCurrentPlayerViewOffsetZ;
 		}
 	}
 }
