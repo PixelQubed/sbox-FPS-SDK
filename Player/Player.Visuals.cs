3be2aafd8c33109d499b9e4c6f8843df185e96c0
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2022-08-16 16:22:13 +0300

    Add ApplyViewPunchImpulse

--- Player/Player.Visuals.cs
+++ Player/Player.Visuals.cs
@@ -13,6 +13,14 @@ partial class Source1Player : IHasParticleContainer
 	[Net, Predicted] public Vector3 ViewPunchAngle { get;  set; }
 	[Net, Predicted] public Vector3 ViewPunchAngleVelocity { get;  set; }
 
+	public virtual void ApplyViewPunchImpulse( float pitch, float yaw = 0, float roll = 0 )
+	{
+		var angle = ViewPunchAngle;
+		angle.x += pitch;
+		angle.y = yaw;
+		angle.z = roll;
+		ViewPunchAngle = angle;
+	}
 
 	public void DecayAngles( ref Vector3 angle, float exp, float lin, float time )
 	{
